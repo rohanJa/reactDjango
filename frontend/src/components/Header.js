a1e02635af2bd27beea15e76d99f2dbe3e2451f2
rohanJa <rohan.jain150300@gmail.com>
2020-12-25 17:55:52 +0530

    Frontend Added

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="text-center">
+        <h1>Web Application using Django REST Framework and React</h1>        
+        <hr />
+      </div>
+    );
+  }
+}
+
+export default Header;
