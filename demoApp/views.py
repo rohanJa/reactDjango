f20a6bc23221243bc7f0b266a1a1593c71bac669
rohanJa <rohan.jain150300@gmail.com>
2020-12-30 23:58:03 +0530

    Updated view added class based view for POST and delete,put method

--- demoApp/views.py
+++ demoApp/views.py
@@ -1,13 +1,18 @@
 # Create your views here.
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework import status
-
 from .models import Student
 from .serializers import *
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from rest_framework import permissions, status
+from rest_framework.views import APIView
 
-@api_view(['GET', 'POST'])
+@api_view(('GET',))
 def students_list(request):
+    print("Roah")
+    # print((vars(request)))
+    print(request.method)
     if request.method == 'GET':
         data = Student.objects.all()
 
@@ -23,20 +28,90 @@ def students_list(request):
             
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['PUT', 'DELETE'])
-def students_detail(request, pk):
-    try:
-        student = Student.objects.get(pk=pk)
-    except Student.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'PUT':
+class StudentDetail(APIView):
+    '''
+        To allow only authenticated user to perform the CRUD operations.
+    '''
+    
+    permission_classes = (permissions.IsAuthenticated,)
+
+
+    def post(self, request):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+            
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        try:
+            student = Student.objects.get(pk=pk)
+        except Student.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
         serializer = StudentSerializer(student, data=request.data,context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(status=status.HTTP_204_NO_CONTENT)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk):
+        try:
+            student = Student.objects.get(pk=pk)
+        except Student.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
         student.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(["GET"])
+def single_student(request, pk):
+        if request.method == 'GET':
+            try:
+                data = Student.objects.get(pk=pk)
+            except Student.DoesNotExist:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+                
+            serializer = StudentSerializer(data, context={'request': request}, many=True)
+
+            return Response(serializer.data)
+
+@api_view(['GET'])
+def current_user(request):
+    """
+    Determine the current user by their token, and return their data
+    """
+    
+    serializer = UserSerializer(request.user)
+    print(serializer.data)
+    return Response(serializer.data)
+
+
+# Create your views here.
+
+@api_view(['GET'])
+def current_user(request):
+    """
+    Determine the current user by their token, and return their data
+    """
+    
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
+
+
+class UserList(APIView):
+    """
+    Create a new user. It's called 'UserList' because normally we'd have a get
+    method here too, for retrieving a list of all User objects.
+    """
+    
+
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = UserSerializerWithToken(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
